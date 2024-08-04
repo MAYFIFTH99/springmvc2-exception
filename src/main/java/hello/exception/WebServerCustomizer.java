ba0c321772eb9afd2c3c1a79541e1df76333fbb1
harvey <alstjr971@naver.com>
2024-08-04 13:57:03 +0900

    MyHandlerExceptionResolver

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -15,7 +15,7 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
                 "/errorpage/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,
                 "/error-page/500");
-        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-                page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
 
 
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
